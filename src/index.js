dd1f5b71a666b2e2bc36624bd45f5d2f8c71a30e
mohammadhazzly <hazly.v@live.com>
2026-05-04 23:52:51 +0300

    Updated website

---
+++ src/index.js
@@ -0,0 +1,19 @@
+﻿import html from './website.html';
+
+export default {
+  async fetch(request, env, ctx) {
+    const url = new URL(request.url);
+    
+    if (url.pathname === '/' || url.pathname === '/index.html') {
+      return new Response(html, {
+        headers: { 'Content-Type': 'text/html; charset=utf-8' },
+        status: 200
+      });
+    }
+    
+    return new Response(html, {
+      headers: { 'Content-Type': 'text/html; charset=utf-8' },
+      status: 200
+    });
+  }
+};
